c3b2778eaa27672f0ed1d53d5a3931f3c09ce12a
solraczo <79017916+solraczo@users.noreply.github.com>
2025-02-11 09:58:40 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.153.0/build/three.module.js';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.153.0/build/three.module.min.js';
 import { ARButton } from 'https://solraczo.github.io/solarandroid/libs/ARButton.js';
 import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.153.0/examples/jsm/loaders/GLTFLoader.js';
 import { RGBELoader } from 'https://cdn.jsdelivr.net/npm/three@0.153.0/examples/jsm/loaders/RGBELoader.js';
