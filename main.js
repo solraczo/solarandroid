6fda40813e4e72bd17703b9653e638a0bb9f8589
solraczo <79017916+solraczo@users.noreply.github.com>
2025-02-11 10:01:25 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.153.0/build/three.module.min.js';
+import * as THREE from 'https://https://solraczo.github.io/solarandroid/libs/three.module.js';
 import { ARButton } from 'https://solraczo.github.io/solarandroid/libs/ARButton.js';
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.153.0/examples/jsm/loaders/GLTFLoader.js';
-import { RGBELoader } from 'https://cdn.jsdelivr.net/npm/three@0.153.0/examples/jsm/loaders/RGBELoader.js';
+import { GLTFLoader } from 'https://https://solraczo.github.io/solarandroid/libs/GLTFLoader.js';
+import { RGBELoader } from 'https://https://solraczo.github.io/solarandroid/libs/RGBELoader.js';
 
 
 let mixerGLTF;
